914e7c31e88a27684f5565c9410d90c2668ca7be
ChyntiaLeonie <chyn_leo038@yahoo.co.id>
2017-05-31 17:00:48 +0700

    Initial

---
+++ googleWithoutCocoapods/AppDelegate.swift
@@ -0,0 +1,75 @@
+//
+//  AppDelegate.swift
+//  googleWithoutCocoapods
+//
+//  Created by Prelo on 5/31/17.
+//  Copyright © 2017 Prelo. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+
+@UIApplicationMain
+// [START appdelegate_interfaces]
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
+    // [END appdelegate_interfaces]
+    var window: UIWindow?
+    
+    // [START didfinishlaunching]
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Initialize sign-in
+        GIDSignIn.sharedInstance().clientID = "1018435039908-f1opejdr81c9suseb1m73uo3r4g83qfi.apps.googleusercontent.com"
+        
+        return true
+    }
+    // [END didfinishlaunching]
+    @available(iOS 9.0, *)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url,
+                                                 sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
+                                                 annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+    }
+    
+    // [START signin_handler]
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        if let error = error {
+            print("\(error.localizedDescription)")
+            // [START_EXCLUDE silent]
+            NotificationCenter.default.post(
+                name: Notification.Name(rawValue: "ToggleAuthUINotification"), object: nil, userInfo: nil)
+            // [END_EXCLUDE]
+        } else {
+            // Perform any operations on signed in user here.
+            let userId = user.userID                  // For client-side use only!
+            let idToken = user.authentication.idToken // Safe to send to the server
+            let fullName = user.profile.name
+            let givenName = user.profile.givenName
+            let familyName = user.profile.familyName
+            let email = user.profile.email
+            
+            // [START_EXCLUDE]
+            NotificationCenter.default.post(
+                name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+                object: nil,
+                userInfo: ["statusText": "Signed in user:\n\(fullName)"])
+            // [END_EXCLUDE]
+            
+        }
+    }
+    // [END signin_handler]
+    // [START disconnect_handler]
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+              withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // [START_EXCLUDE]
+        NotificationCenter.default.post(
+            name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+            object: nil,
+            userInfo: ["statusText": "User has disconnected."])
+        // [END_EXCLUDE]
+    }
+    // [END disconnect_handler]
+}
